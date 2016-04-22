f046ad79f4ca00717fa93603fe0c159ec5264c20
Thomas Gatterer <thomas.gatterer@uibk.ac.at>
2016-04-22 06:36:08 +0200

    Adjusted Arduino things

--- laus/scriptsForClasses/UBUNTU1404/170-installArduino.sh
+++ laus/scriptsForClasses/UBUNTU1404/170-installArduino.sh
@@ -6,10 +6,12 @@
 # source configuration
 . ../OPTIONS.conf
 
-
+######Arduino##########################################################
 #http://labs.arduino.org/Arduino%20IDE%20on%20Linux-based%20OS
-
-cp -v files/arduino-1.7.10.org-linux64.tar.xz /usr/lib
+cd /tmp/
+wget http://download.arduino.org/IDE/1.7.10/arduino-1.7.10.org-linux64.tar.xz
+tar -xvf arduino-1.7.10.org-linux64.tar.xz
+cp /arduino-1.7.10-linux64 /usr/lib/arduino-1.7.10-linux64
 ln /usr/bin/arduino /usr/lib/arduino-1.7.10-linux64/arduino
 
 # manipulated file
@@ -28,4 +30,19 @@ usermod -a -G dialout worker
 
 apt-get remove modemmanager
 
-reboot
+#####S4A##############################################################
+# http://s4a.cat/
+cd /tmp
+wget http://vps34736.ovh.net/S4A/S4A16.deb
+
+dpkg --add-architecture i386
+apt-get update
+apt-get install ia32-libs
+
+dpkg -i --force-architecture S4A16.deb
+
+## bring Firmaware to desktop
+cd /home/worker/Schreibtisch/
+wget http://vps34736.ovh.net/S4A/S4AFirmware16.ino
+
+#reboot
