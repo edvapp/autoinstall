167c51fd1d038f00c850b6223cb664cbaeec4af0
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2021-03-01 13:57:34 +0100

    add set-apt-cacher to update-script

--- laus/scriptsForClasses/INSTALL_U2004/010-update.sh
+++ laus/scriptsForClasses/INSTALL_U2004/010-update.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#! /bin/bash
 
+
+## we create /etc/apt/apt.conf 
+## the same way a 
+## pxe - preseed 
+## installation creats its files
+
+if [ ! -f /etc/apt/apt.conf ] ;
+then
+	echo 'Acquire::http { Proxy "http://apca01:3142"; };' > /etc/apt/apt.conf
+	chmod 644 /etc/apt/apt.conf
+fi
 
 ## ONE single update here for all following packages
 apt-get -y update
