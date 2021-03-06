31fe861015d2520a5e36a98c9079026945b0edee
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2021-03-06 14:04:24 +0100

    new license key for virtualbox-extensions-pack

--- laus/scriptsForClasses/INSTALL_U2004/121-installVirtualBoxExtensionpack.sh
+++ laus/scriptsForClasses/INSTALL_U2004/121-installVirtualBoxExtensionpack.sh
@@ -8,4 +8,6 @@ echo $file
 wget download.virtualbox.org/virtualbox/${VERSION_NUMBER}/${file} -O /tmp/${file}
 
 VBoxManage extpack uninstall "Oracle VM VirtualBox Extension Pack"
-VBoxManage extpack install /tmp/${file} --replace --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
+#VBoxManage extpack install /tmp/${file} --replace --accept-license=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9eb
+# new license key: 2021.03.06
+VBoxManage extpack install /tmp/${file} --replace --accept-license=33d7284dc4a0ece381196fda3cfe2ed0e1e8e7ed7f27b9a9ebc4ee22e24bd23c
