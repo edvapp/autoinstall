845066a959d2b98831c000f1327b4f386a340ce4
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2021-03-16 17:19:26 +0100

    add german language pack, because of ubuntu-server-installer

--- laus/scriptsForClasses/INSTALL_U2004/020-install_UBUNTU_Desktop.sh
+++ laus/scriptsForClasses/INSTALL_U2004/020-install_UBUNTU_Desktop.sh
@@ -6,10 +6,11 @@
 export DEBIAN_FRONTEND=noninteractive
 
 ## Full Ubuntu - GNOME - Desktop
+## german language packs
 ## dconf-editor: editor to customize hidden desktop options
 ## gnome-shell-extension: program to control shell extensions
 ## compizconfig-settings-manager: more Settings for Compiz Graphic Manager
 ## chrome-gnome-shell: backend to install gnome-extensions via browser
 ## firefox-locale-de: german language pack firefox
 ## mc: Midnight commander
-apt-get -y install ubuntu-desktop dconf-editor gnome-shell-extensions compizconfig-settings-manager chrome-gnome-shell firefox-locale-de mc
+apt-get -y install ubuntu-desktop language-pack-de-base language-pack-de language-pack-gnome-de-base language-pack-gnome-de dconf-editor gnome-shell-extensions compizconfig-settings-manager chrome-gnome-shell firefox-locale-de mc
