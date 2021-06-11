34e43d51e6b6f629d24b68fd137531201f43562a
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2021-06-11 15:20:19 +0200

    fix missing dir in makeDesktopLinkforTestees.sh

--- laus/scriptsForClasses/CONFIG_U2104/files/makeDesktopLinkForTestees.sh
+++ laus/scriptsForClasses/CONFIG_U2104/files/makeDesktopLinkForTestees.sh
@@ -6,7 +6,7 @@ then
 	SCHOOL_NUMBER=${USER:7:6}
 	if [ ! -d ${HOME}/Abgabe_$USER ];
 	then
-		ln -s /home/users/${SCHOOL_NUMBER}/${USER} ${HOME}/Abgabe_${USER}
+		ln -s /home/users/${SCHOOL_NUMBER}/t/${USER} ${HOME}/Abgabe_${USER}
 	fi
 
 	if [ ! -d ${HOME}/Abgabe_Vorlagen ];
@@ -16,7 +16,7 @@ then
 
 	if [ ! -d ${HOME}/Schreibtisch/Abgabe_${USER} ];
 	then
-		ln -s /home/users/${SCHOOL_NUMBER}/${USER} ${HOME}/Schreibtisch/Abgabe_${USER}
+		ln -s /home/users/${SCHOOL_NUMBER}/t/${USER} ${HOME}/Schreibtisch/Abgabe_${USER}
 	fi
 
 	if [ ! -d ${HOME}/Schreibtisch/Vorlagen ];
